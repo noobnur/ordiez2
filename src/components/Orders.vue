adcc0cf1f7aca1126abeba00d0c315171631d9bc
noobnur <nurhsans@gmail.com>
2017-11-28 18:26:51 +0800

    orders pulled from server successfully

---
+++ src/components/Orders.vue
@@ -0,0 +1,45 @@
+<!-- src/components/Orders.vue -->
+<template>
+<table class="table table-hover product-table">
+  <thead>
+    <tr>
+      <th>Order ID</th>
+      <th>Feedback</th>
+    </tr>
+  </thead>
+  <tbody>
+    <!-- <tr v-for="product in products" track-by="id">
+        <td>{{orders}}</td>
+        <td>Feedback</td>
+      </tr> -->
+
+      <p>
+        {{orders}}
+      </p>
+  </tbody>
+</table>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      orders: [],
+      errors: []
+    }
+  },
+
+  created() {
+axios.get('https://stormy-earth-22350.herokuapp.com/orders')
+.then(response => {
+  this.orders = response.data
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+
+}
+}
+</script>
